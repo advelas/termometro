9ae197042a5fcc407c6d3134c57632207c20a9df
Alejandro Danel Velasquez <81099699+advelas@users.noreply.github.com>
2021-05-08 12:56:27 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 let mes=parseInt(prompt("¿Cuantos días tendrá este mes?"));
 let registro=1;
-let dato=[];
+let datoP=[];
+let datoN=[];
 
 function tomarDato(){
-dato.push(parseInt(prompt(`Día ${registro} (de ${mes}). Exprese la temperatura de hoy (en números)`)));
-registro = (registro+1)
+let entrada= parseInt(prompt(`Día ${registro} de ${mes}. ¿Cual es la temperatura de hoy?`));
+
+if (entrada >= 0) {
+datoP.push(entrada);
+entrada=0;
+} else {
+datoN.push(entrada);
+entrada=0;
+}
+
+registro = (registro+1);
 }
 
 do {
 	tomarDato()
 } while(registro<=mes);
 
-document.write("Temperaturas: " + "<br>" + dato);
+let suma = datoN.reduce((current, previous) => current += previous);
+let promedio = suma / datoN.length;
+let tempMinima= Math.min.apply(null, datoN);
+let tempMaxima= Math.max.apply(null, datoP);
 
-document.write("<br>" + parseFloat(- dato));
+document.write("Temperaturas sobre 0: " + "<br>" + datoP + "<br>");
+document.write("Temperaturas bajo 0: " + "<br>" + datoN + "<br>");
+document.write("Promedio de temperaturas bajo 0: " + "<br>" + promedio + "<br>");
+document.write("Temperatura máxima: " + tempMaxima + "<br>");
+document.write("Temperatura mínima: " + tempMinima + "<br>");
